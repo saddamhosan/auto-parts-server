4ae8fb84837899c7f50395895a6ad495574c9674
saddamhosan <contact.saddamhosan.com>
2022-05-22 03:53:20 +0600

    added jwt

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require('dotenv').config()
+var jwt = require("jsonwebtoken");
 const app=express()
 const port=process.env.PORT ||4000
 
@@ -20,22 +21,44 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
-        await client.connect();
-        const partsCollection = client.db("automobile").collection("parts");
-        //get all parts
-        app.get('/parts',async(req,res)=>{
-            const result= await partsCollection.find().toArray()
-            res.send(result)
-        })
-        //get one part find by id
-        app.get('/part/:id',async(req,res)=>{
-            const id=req.params
-            const query={_id:ObjectId(id)}
-            const result= await partsCollection.findOne(query)
-            res.send(result)
-        })
-    
-  } finally {
+      await client.connect();
+      const partsCollection = client.db("automobile").collection("parts");
+      const userCollection = client.db("automobile").collection("user");
+
+      //when user sign in and login then get a token
+      app.put("/user/:email", async (req, res) => {
+        const email = req.params.email;
+        const user = req.body;
+        console.log(user)
+        const query = { email };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: user,
+        };
+        const result = await userCollection.updateOne(
+          query,
+          updateDoc,
+          options
+        );
+        const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "1h",
+        });
+        res.send({ result, token });
+      });
+
+      //get all parts
+      app.get("/parts", async (req, res) => {
+        const result = await partsCollection.find().toArray();
+        res.send(result);
+      });
+      //get one part find by id
+      app.get("/part/:id", async (req, res) => {
+        const id = req.params;
+        const query = { _id: ObjectId(id) };
+        const result = await partsCollection.findOne(query);
+        res.send(result);
+      });
+    } finally {
     // await client.close();
   }
 }
