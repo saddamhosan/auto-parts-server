80b97ba0fa97cd935e58262d11ead25b27df335f
saddamhosan <contact.saddamhosan.com>
2022-05-22 00:22:50 +0600

    added get api for all parts

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require('dotenv').config()
 const app=express()
 const port=process.env.PORT ||4000
@@ -10,6 +11,31 @@ app.use(cors())
 app.use(express.json())
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2kmk8.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const partsCollection = client.db("automobile").collection("parts");
+        //get all parts
+        app.get('/parts',async(req,res)=>{
+            const result= await partsCollection.find().toArray()
+            res.send(result)
+        })
+    
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+//https://pacific-hamlet-76531.herokuapp.com/
 app.get('/',(req,res)=>{
     res.send("welcome to automobile manufacturing server");
 
