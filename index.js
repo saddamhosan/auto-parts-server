c5c18a4c1d78fef9b7babe3fcdc6bf8ec04be349
saddamhosan <contact.saddamhosan.com>
2022-05-23 00:02:00 +0600

    added admin rul

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-require('dotenv').config()
+require("dotenv").config();
 var jwt = require("jsonwebtoken");
-const app=express()
-const port=process.env.PORT ||4000
-
+const app = express();
+const port = process.env.PORT || 4000;
 
 //middleware
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2kmk8.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -20,93 +18,112 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    try {
-      await client.connect();
-      const partsCollection = client.db("automobile").collection("parts");
-      const userCollection = client.db("automobile").collection("user");
-      const orderCollection = client.db("automobile").collection("order");
-      const reviewCollection = client.db("automobile").collection("review");
+  try {
+    await client.connect();
+    const partsCollection = client.db("automobile").collection("parts");
+    const userCollection = client.db("automobile").collection("user");
+    const orderCollection = client.db("automobile").collection("order");
+    const reviewCollection = client.db("automobile").collection("review");
 
-      //when user sign in and login then get a token
-      app.put("/user/:email", async (req, res) => {
-        const email = req.params.email;
-        const user = req.body;
-        const query = { email };
-        const options = { upsert: true };
-        const updateDoc = {
-          $set: user,
-        };
-        const result = await userCollection.updateOne(
-          query,
-          updateDoc,
-          options
-        );
-        const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, {
-          expiresIn: "1h",
-        });
-        res.send({ result, token });
+    //when user sign in and login then get a token
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const query = { email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(query, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
       });
+      res.send({ result, token });
+    });
 
-      //get all parts
-      app.get("/parts", async (req, res) => {
-        const result = await partsCollection.find().toArray();
-        res.send(result);
-      });
-      //get one part find by id
-      app.get("/part/:id", async (req, res) => {
-        const id = req.params;
-        const query = { _id: ObjectId(id) };
-        const result = await partsCollection.findOne(query);
-        res.send(result);
-      });
+    //added admin role
+    app.put("/user/admin/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const updateDoc = {
+        $set: { role: "admin" },
+      };
+      const result = await userCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
 
-      // insert a order
-      app.post("/order", async (req, res) => {
-        const order = req.body;
-        const result = await orderCollection.insertOne(order);
-        res.send(result);
-      });
+    //get all users
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
 
-      //get order filtering by email
-      app.get("/order/:email", async (req, res) => {
-        const email = req.params.email;
-        const query = { email };
-        const result = await orderCollection.find(query).toArray();
-        res.send(result);
-      });
+    //delete user
+    app.delete("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = {email};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
 
-      //delete order
-      app.delete("/order/:id", async (req, res) => {
-        const id = req.params.id;
-        const query = { _id: ObjectId(id) };
-        const result = await orderCollection.deleteOne(query);
-        res.send(result);
-      });
+    //get all parts
+    app.get("/parts", async (req, res) => {
+      const result = await partsCollection.find().toArray();
+      res.send(result);
+    });
+    //get one part find by id
+    app.get("/part/:id", async (req, res) => {
+      const id = req.params;
+      const query = { _id: ObjectId(id) };
+      const result = await partsCollection.findOne(query);
+      res.send(result);
+    });
 
-      //insert a review
-      app.post("/review", async (req, res) => {
-        const review = req.body;
-        const result = await reviewCollection.insertOne(review);
-        res.send(result);
-      });
-      //get all review
-      app.get("/review", async (req, res) => {
-        const result = await reviewCollection.find().toArray();
-        res.send(result);
-      });
-    } finally {
+    // insert a order
+    app.post("/order", async (req, res) => {
+      const order = req.body;
+      const result = await orderCollection.insertOne(order);
+      res.send(result);
+    });
+
+    //get order filtering by email
+    app.get("/order/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const result = await orderCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    //delete order
+    app.delete("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    //insert a review
+    app.post("/review", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
+    //get all review
+    app.get("/review", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+  } finally {
     // await client.close();
   }
 }
 run().catch(console.dir);
 
-
 //https://pacific-hamlet-76531.herokuapp.com/
-app.get('/',(req,res)=>{
-    res.send("welcome to automobile manufacturing server");
+app.get("/", (req, res) => {
+  res.send("welcome to automobile manufacturing server");
+});
 
-})
-
-app.listen(port,()=>{
-    console.log('listening to tort',port);
-})
+app.listen(port, () => {
+  console.log("listening to tort", port);
+});
